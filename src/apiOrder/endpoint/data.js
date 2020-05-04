5822e6f006e723a3503b2f74bf0f1a58792dda20
Ali Mustofa <alimustofa97@gmail.com>
2020-05-05 01:06:18 +0700

    try apidoc

--- src/apiOrder/endpoint/data.js
+++ src/apiOrder/endpoint/data.js
@@ -4,6 +4,45 @@ const grpc = require('grpc')
 const { Tags, FORMAT_HTTP_HEADERS } = require('opentracing')
 const tracer = require('../../../utils/tracer')
 
+/**
+ * @api {get} /order/:orderCode GetOrder
+ * @apiName GetOrderData
+ * @apiGroup Order
+ * @apiVersion 1.0.0
+ * 
+ * @apiDescription 
+ * Get order data
+ *
+ * @apiParam {String} orderCode Order Code (9001-00930094).
+ *
+ * @apiSuccess {Number} status Response status.
+ * @apiSuccess {String} code  Response code.
+ * @apiSuccess {String} message  Message of the endpoint.
+ * @apiSuccess {Object[]} data  Data of the response.
+ * @apiSuccess {Number} data.id  ID of order.
+ * @apiSuccess {String} data.orderCode  OrderCode of order.
+ * 
+ * 
+ * @apiSuccessExample {json} Success-Response:
+ *     {
+ *       "status": 200,
+ *       "code": null,
+ *       "message": "Order Data",
+ *       "data": [
+ *          {
+ *              "id": 1000000001,
+ *              "orderCode": "8001-00000001"
+ *          }
+ *       ]
+ *     }
+ * 
+ * @apiError UserNotFound The <code>id</code> of the User was not found.
+ * 
+ * @apiErrorExample {json} Error-Response:
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
 module.exports = (service) => async (req, res) => {
     const apiDataOrderSpan = tracer.startSpan("order-data")
 
